8b5cdd2b8eac184da5435761cd0e1ed11e2336d8
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-18 07:15:40 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community_flexiblelayout/trunk@11179 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ controller/class.tx_communityflexiblelayout_controller_dashboard.php
@@ -0,0 +1,57 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2008 Frank Nägler <typo3@naegler.net>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * User Profile Application Controller
+ *
+ * @author	Frank Nägler <typo3@naegler.net>
+ * @package TYPO3
+ * @subpackage community_flexiblelayout
+ */
+class tx_communityflexiblelayout_controller_Dashboard {
+	public $prefixId      = 'tx_communityflexiblelayout_controller_Dashboard';		// Same as class name
+	public $scriptRelPath = 'controller/class.tx_communityflexiblelayout_controller_dashboard.php';	// Path to this script relative to the extension dir.
+	public $extKey        = 'community_flexiblelayout';	// The extension key.
+
+	public $cObj;
+
+	/**
+	 * constructor for class tx_communityflexiblelayout_controller_Dashboard
+	 */
+	public function __construct() {
+
+	}
+
+	public function execute($content, array $configuration) {
+		return 'hello dashboard';
+	}
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MOlDE]['XCLASS']['ext/community_flexiblelayout/controller/class.tx_communityflexiblelayout_controller_dashboard.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/community_flexiblelayout/controller/class.tx_communityflexiblelayout_controller_dashboard.php']);
+}
+
+?>
