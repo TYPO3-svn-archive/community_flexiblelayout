1b5570c41bc6cd55052c3a58983f804933259c55
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-12 17:39:45 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community_flexiblelayout/trunk@11914 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,15 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+$TX_COMMUNITY['applications']['StartPage'] = array(
+	'classReference' => 'EXT:community/controller/class.tx_community_controller_userprofileapplication.php:tx_community_controller_UserProfileApplication',
+	'label' => 'LLL:EXT:community/lang/locallang_applications.xml:userProfile',
+	'accessControl' => array(
+		'read' => 'LLL:EXT:community/lang/locallang_privacy.xml:privacy_userProfile_read'
+	),
+	'widgets' => array(
+	)
+);
 
 
 ?>
