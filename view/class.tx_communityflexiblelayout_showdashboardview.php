edec0e3ee8a374c146eb130cde969c110c11d5e3
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-17 11:35:31 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community_flexiblelayout/trunk@13074 735d13b6-9817-0410-8766-e36946ffe9aa

--- view/class.tx_communityflexiblelayout_showdashboardview.php
+++ view/class.tx_communityflexiblelayout_showdashboardview.php
@@ -79,8 +79,8 @@ class tx_communityflexiblelayout_ShowDashboardView {
 				foreach ($widgetsArray as $widgetName => $widget) {
 					$widgetClasses = array();
 					$widgetClasses[] = 'widget';
-					//$widgetClasses[] = ($widget->isDraggable()) ? 'draggable' : '';
-					//$widgetClasses[] = ($widget->isRemovable()) ? 'removable' : '';
+					$widgetClasses[] = ($widget->isDraggable()) ? 'draggable' : 'undraggable';
+					$widgetClasses[] = ($widget->isRemovable()) ? 'removable' : '';
 					$widgetClasses[] = ($widget->getCssClass()) ? $widget->getCssClass() : '' ;
 					$widgetMarker = array(
 						'WIDGET_LABEL'	=> $widget->getLabel(),
