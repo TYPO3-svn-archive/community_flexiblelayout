5dc1b4a5efe8cd0f30220bfb7a01915a67d60234
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-20 07:43:58 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community_flexiblelayout/trunk@13153 735d13b6-9817-0410-8766-e36946ffe9aa

--- hooks/class.tx_communityflexiblelayout_hook_community.php
+++ hooks/class.tx_communityflexiblelayout_hook_community.php
@@ -48,27 +48,87 @@ class tx_communityflexiblelayout_hook_Community implements tx_community_UserProf
 		
 		$this->configuration = $this->profileActionsWidget->getConfiguration();
 		
-		$profileActions[] = $this->getAddAsScoolmateLink();
+		$links = $this->getScoolmateLinks();
+		foreach ($links as $link) {
+			$profileActions[] = array('link', $link);	
+		}
 		return $profileActions;
 	}
 	
-	protected function getAddAsScoolmateLink() {
-		
+	protected function getScoolmateLinks() {
 		// @TODO: check if there is a relatiion with this role, change link to remove, edit, etc,
+		$returnData = array();
 		
-		$content = $this->profileActionsWidget->getCommunityApplication()->pi_linkToPage(
-			$this->localizationManager->getLL('action_addAsScoolmate'),
-			$GLOBALS['TSFE']->id,
-			'',
-			array(
-				'tx_community' => array(
-					'user' => $this->profileActionsWidget->getCommunityApplication()->getRequestedUser()->getUid(),
-					'profileActionsAction' => 'addAsFriend',
-					'roleId' => 3
+		$requestedUser  = $this->profileActionsWidget->getCommunityApplication()->getRequestedUser();
+		$requestingUser = $this->profileActionsWidget->getCommunityApplication()->getRequestingUser();
+		
+		if ($this->isScoolmate($requestingUser, $requestedUser)) {
+			$returnData[] = $this->profileActionsWidget->getCommunityApplication()->pi_linkToPage(
+				$this->localizationManager->getLL('action_removeAsFriend'),
+				$GLOBALS['TSFE']->id,
+				'',
+				array(
+					'tx_community' => array(
+						'user' => $this->profileActionsWidget->getCommunityApplication()->getRequestedUser()->getUid(),
+						'profileActionsAction' => 'removeAsFriend',
+						'roleId' => 3
+					)
 				)
-			)
-		);
-		return array('link' => $content);
+			); 
+			$returnData[] = $this->profileActionsWidget->getCommunityApplication()->pi_linkToPage(
+				$this->localizationManager->getLL('action_editRelationship'),
+				$GLOBALS['TSFE']->id,
+				'',
+				array(
+					'tx_community' => array(
+						'user' => $this->profileActionsWidget->getCommunityApplication()->getRequestedUser()->getUid(),
+						'profileActionsAction' => 'editRelationship',
+						'roleId' => 3
+					)
+				)
+			); 
+		} else {
+			$returnData[] = $this->profileActionsWidget->getCommunityApplication()->pi_linkToPage(
+				$this->localizationManager->getLL('action_addAsFriend'),
+				$GLOBALS['TSFE']->id,
+				'',
+				array(
+					'tx_community' => array(
+						'user' => $this->profileActionsWidget->getCommunityApplication()->getRequestedUser()->getUid(),
+						'profileActionsAction' => 'addAsFriend',
+						'roleId' => 3
+					)
+				)
+			); 
+		}
+		
+		
+		return $returnData;
 	}
+
+	protected function isScoolmate(tx_community_model_User $user, tx_community_model_User $friend) {
+			// TODO this schould at some time be moved to a more appropriate place like a FriendshipManager or so
+			// TODO: Question: I think this should be a method of the user object: isInRelationTo($user, $role) ?
+		$isFriend = false;
+
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+			'f1.uid',
+			'tx_community_friend AS f1 JOIN tx_community_friend AS f2
+				ON f1.feuser = f2.friend AND f1.friend = f2.feuser
+				AND f1.feuser = ' . $user->getUid()
+			. ' AND f1.friend = ' . $friend->getUid()
+			. ' AND f1.role = 3',
+			''
+		);
+
+		$friendConnectionCount = $GLOBALS['TYPO3_DB']->sql_num_rows($res);
+
+		if ($friendConnectionCount > 0) {
+			$isFriend = true;
+		}
+
+		return $isFriend;
+	}
+	
 }
 ?>
