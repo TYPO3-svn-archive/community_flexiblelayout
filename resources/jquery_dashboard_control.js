1c0ef960126f3ab99d2f88e13952a5ec060e5c69
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-03 11:11:36 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community_flexiblelayout/trunk@13676 735d13b6-9817-0410-8766-e36946ffe9aa

--- resources/jquery_dashboard_control.js
+++ resources/jquery_dashboard_control.js
@@ -53,6 +53,7 @@
 				'handle': '.label',
 				'cancel': '.undraggable',
 				'cursor': 'crosshair',
+				'placeholder': 'widget-placeholder',
 				'over': function(e, ui) {
 					if (ui.element.attr('id') == 'tx-communityflexiblelayout-clipboard') {
 						ui.helper.find('.label').show();
@@ -64,6 +65,32 @@
 				},
 				'out': function(e, ui) {
 					ui.element.removeClass('ui-sortable-hover');
+				},
+				'start': function(e, ui) {
+					ui.placeholder
+						.width(ui.helper.width())
+						.height(ui.helper.height()); 
+				},
+				'stop': function(e, ui) {
+					ui.placeholder
+						.width("auto")
+						.height("auto"); 
+				},
+				'sort': function(e, ui) {
+					ui.placeholder
+						.width(ui.helper.width())
+						.height(ui.helper.height()); // maintain size of placeholder when ui.item is repositioned 
+					// console.log(ui.item);
+					// console.log(ui.helper);
+					// $(ui.item).height();
+					// ;
+					/*
+					$(ui.item).parent().find('.widget-placeholder').each(function(element) {
+						element.width($(ui.item).width());
+						element.height($(ui.item).height());
+					});
+					*/
+					
 				},
 				'update': function(e, ui) {
 					if (getParam('tx_community[group]').length) {
