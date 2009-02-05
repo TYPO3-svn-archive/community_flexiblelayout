b6110f7add927122b5ac3b0a11b7cc707fda9c21
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-05 15:05:13 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community_flexiblelayout/trunk@16498 735d13b6-9817-0410-8766-e36946ffe9aa

--- controller/class.tx_communityflexiblelayout_controller_coaspecialwidget.php
+++ controller/class.tx_communityflexiblelayout_controller_coaspecialwidget.php
@@ -39,7 +39,7 @@ class tx_communityflexiblelayout_controller_CoaspecialWidget extends tx_communit
 		$this->localizationManager = tx_community_LocalizationManager::getInstance('EXT:community_flexiblelayout/lang/locallang_application.xml', $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_communityflexiblelayout.']);
 		
 		$this->name     = 'coaspecialWidget';
-		$this->label    = $this->localizationManager->getLL('label_coaspecialWidget');
+		$this->label    = $this->localizationManager->getLL('label_'.$this->name);
 		if($GLOBALS['TSFE']->fe_user->user['tx_communityflexiblelayout_lotteryonly']){
 			$this->label    = $this->localizationManager->getLL('label_coaspecialWidgetLottery');
 		}
